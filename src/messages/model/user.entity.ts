145f904a21f3e5868f6bab412eae32e3729c2b63
kornel <masterkkk@o2.pl>
2024-12-30 02:20:18 +0100

    add broken OCC

--- src/messages/model/user.entity.ts
+++ src/messages/model/user.entity.ts
@@ -1,5 +1,11 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { UserDto } from "./user.dto";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  VersionColumn,
+} from 'typeorm';
+import { UserDto } from './user.dto';
 
 @Entity('user_table')
 // @Unique('constraint_name', ['col_one', 'col_two'])
@@ -14,4 +20,11 @@ export class User {
   email: string;
   @Column()
   password: string;
+
+  // http://typeorm.io/select-query-builder#set-locking
+  // ? https://github.com/typeorm/typeorm/issues/2848#issuecomment-443143839
+  // https://github.com/typeorm/typeorm/issues/3608
+  @VersionColumn() // TODO check if it issues incorrect UPDATE statements on Postgres (lacks version in where clause)
+  // i.e. UPDATE "user_table" SET "email" = ?, "version" = "version" + 1 WHERE "id" IN (11)
+  version: number;
 }
